47de4d33b3bd45d72424642686d1365ea6872a01
Vicest42 <quiqueecheverria10@hotmail.com>
2024-07-18 20:38:22 -0600

    Se añaden imagenes necesarias para proyecto y se configura el enrutador

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import InicioView from "@/views/InicioView.vue";
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    
-  ]
+ history: createWebHistory(import.meta.env.BASE_URL),
+ routes: [
+   {
+     path: '/',
+     name: 'inicio',
+     component: InicioView
+   },
+   {
+     path: '/catalogo',
+     name: 'catalogo',
+     component: () => import('@/views/CatalogoView.vue')
+   }
+ ]
 })
+
 
 export default router
