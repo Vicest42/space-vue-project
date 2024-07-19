161e2585485a78857c7d3eaba91cf9e48cb6c3c5
alanaviles12 <143649852+alanaviles12@users.noreply.github.com>
2024-07-19 01:09:17 -0600

    Se agrega a APIService la funcion de buscarPorCategoria

--- src/services/APIService.js
+++ src/services/APIService.js
@@ -1,13 +1,19 @@
 import api from "@/lib/axios.js";
 
+
 export default {
+
+
    catalogoAstros(){
        return api('/')
    },
    buscarAstros(busqueda){
-       return api(`/${busqueda.nombre}`)
+       return api(`/${busqueda}`)
    },
    buscarPorID(id){
        return api(`/${id}`)
+   },
+   buscarPorCategoria(categoria){
+       return api(`?filter[]=bodyType,eq,${categoria}`)
    }
 };
