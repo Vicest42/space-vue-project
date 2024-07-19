89c7f13ade23a4817f20fc3d1eaa91a5a4f16aae
Vicest42 <quiqueecheverria10@hotmail.com>
2024-07-19 00:55:45 -0600

    se agrega la apertura y cierre de ventana modal y se crea la funciòn de busqueda por categoria

--- src/stores/astros.js
+++ src/stores/astros.js
@@ -4,92 +4,113 @@ import { ref, reactive } from 'vue';
 import { defineStore } from 'pinia';
 import APIService from '@/services/APIService.js';
 import NASAService from '@/services/NASAService.js';
-
+import { useModalStore } from "@/stores/modal.js";
 
 export const useAstrosStore = defineStore('astros', () => {
-   const busqueda = reactive({
-       nombre: '',
-       categoria: ''
-   });
+    const busqueda = reactive({
+        nombre: '',
+        categoria: ''
+    });
 
+    const astros = ref([]);
+    const astro = ref({})
+    const images = ref({}); // Nueva ref para almacenar imágenes
 
-   const astros = ref([]);
-   const images = ref({}); // Nueva ref para almacenar imágenes
+    const modalStore = useModalStore();
 
+    const categorias = [
+        { categoria: 'Star' },
+        { categoria: 'Planet' },
+        { categoria: 'Dwarf Planet' },
+        { categoria: 'Asteroid' },
+        { categoria: 'Comet' },
+        { categoria: 'Moon' }
+    ];
 
-   const categorias = [
-       { categoria: 'All' },
-       { categoria: 'Star' },
-       { categoria: 'Planet' },
-       { categoria: 'Dwarf Planet' },
-       { categoria: 'Asteroid' },
-       { categoria: 'Comet' },
-       { categoria: 'Moon' }
-   ];
+    async function obtenerAstros() {
+        if (busqueda.nombre === '' && busqueda.categoria === ''){
+            try {
+                const { data } = await APIService.catalogoAstros();
+                if (data && data.bodies) {
+                    astros.value = data.bodies;
+                    // Obtén imágenes para cada cuerpo celeste
+                    for (const astro of data.bodies) {
+                        await obtenerImagen(astro.englishName);
+                    }
+                } else {
+                    astros.value = [];
+                }
+            } catch (error) {
+                console.error('Error al obtener astros:', error);
+                astros.value = [];
+            }
+        } else if(busqueda.nombre !== ''){
+            try {
+                const { data } = await APIService.buscarAstros(busqueda.nombre);
+                if (data) {
+                    astros.value = [data];
+                    // Obtén imagen para el cuerpo celeste buscado
+                    await obtenerImagen(data.englishName);
+                } else {
+                    astros.value = [];
+                }
+            } catch (error) {
+                console.error('Error al obtener astros:', error);
+                astros.value = [];
+            }
+        } else if(busqueda.nombre === '' && busqueda.categoria !== '') {
+            try {
+                const { data } = await  APIService.buscarPorCategoria(busqueda.categoria)
 
+                if(data && data.bodies) {
+                    astros.value = data.bodies;
 
-   async function obtenerAstros() {
-       if (busqueda.nombre === '') {
-           try {
-               const { data } = await APIService.catalogoAstros();
-               if (data && data.bodies) {
-                   astros.value = data.bodies;
-                   // Obtén imágenes para cada cuerpo celeste
-                   for (const astro of data.bodies) {
-                       await obtenerImagen(astro.englishName);
-                   }
-               } else {
-                   astros.value = [];
-               }
-           } catch (error) {
-               console.error('Error al obtener astros:', error);
-               astros.value = [];
-           }
-       } else {
-           try {
-               const { data } = await APIService.buscarAstros(busqueda);
-               if (data) {
-                   astros.value = [data];
-                   // Obtén imagen para el cuerpo celeste buscado
-                   await obtenerImagen(data.englishName);
-               } else {
-                   astros.value = [];
-               }
-           } catch (error) {
-               console.error('Error al obtener astros:', error);
-               astros.value = [];
-           }
-       }
-   }
+                    for (const astro of data.bodies) {
+                        await obtenerImagen(astro.englishName);
+                    }
+                } else {
+                    astros.value = [];
+                }
+            } catch (error) {
+                console.error('Error al obtener astros:', error);
+                astros.value = [];
+            }
+        }
+    }
 
+    async function obtenerImagen(nombre) {
+        try {
+            const { data } = await NASAService.searchImages(nombre);
+            if (data && data.collection && data.collection.items.length > 0) {
+                images.value[nombre] = data.collection.items[0].links[0].href;
+            } else {
+                images.value[nombre] = '/img/home/bg-inicio.jpg'; // Imagen por defecto
+            }
+        } catch (error) {
+            console.error('Error al obtener imagen:', error);
+            images.value[nombre] = '/img/home/bg-inicio.jpg'; // Imagen por defecto
+        }
+    }
 
-   async function obtenerImagen(nombre) {
-       try {
-           const { data } = await NASAService.searchImages(nombre);
-           if (data && data.collection && data.collection.items.length > 0) {
-               images.value[nombre] = data.collection.items[0].links[0].href;
-           } else {
-               images.value[nombre] = '/img/home/bg-inicio.jpg'; // Imagen por defecto
-           }
-       } catch (error) {
-           console.error('Error al obtener imagen:', error);
-           images.value[nombre] = '/img/home/bg-inicio.jpg'; // Imagen por defecto
-       }
-   }
+    async function seleccionarAstro(id) {
+        const { data } = await APIService.buscarPorID(id)
+        if(data){
+            astro.value = data
+            // Obtén imagen para el cuerpo celeste buscado
+            await obtenerImagen(data.englishName);
+        }
 
+        console.log(astro.value);
+        modalStore.handleClickModal()
+    }
 
-   async function seleccionarAstro(id) {
-       const { data } = await APIService.buscarPorID(id)
-       console.log(data);
-   }
-
-
-   return {
-       categorias,
-       busqueda,
-       obtenerAstros,
-       astros,
-       images,
-       seleccionarAstro
-   };
+    return {
+        categorias,
+        busqueda,
+        obtenerAstros,
+        astros,
+        images,
+        seleccionarAstro,
+        astro
+    };
 });
